586d1e99c71eb47b80a5f4f78e19e3cb7fb7735c
AntDSA <antoinedsa.lycee@gmail.com>
2026-04-16 10:18:37 +0200

    Delete player.py

--- player.py
+++
@@ -1,9 +0,0 @@
-import pygame as p
-
-# caractéristique de notre personnage
-# il n'aura que les PV et un blockNote qui résume ce qu'il possède comme indice
-
-class player():
-    def __init__(self):
-        self.health = 50
-        self.something = None

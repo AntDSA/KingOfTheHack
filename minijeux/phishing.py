fe17b6ed8046a4bdc2711ac3710a942fbe413f86
Léo <leo.tran0108@gmail.com>
2026-04-17 21:42:56 +0200

    vsy, je vais faire ça demain A+

---
+++ minijeux/phishing.py
@@ -0,0 +1,9 @@
+import pygame as p
+import game as g
+import random
+from setting import Setting
+class phishing():
+    def __init__(self):
+        self.screen = (300, 200)
+        self.time = random.randint(2, 5)
+    def 

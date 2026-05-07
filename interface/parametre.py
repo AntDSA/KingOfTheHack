0fe7676c6b41febcc0ee81165f465b1966b0e8a8
inesvallee2003-glitch <inesvallee2003@gmail.com>
2026-05-07 09:11:04 +0200

    Comment out code and mission in parametre.py
    Comment out existing code and mission statement in parametre.py

--- interface/parametre.py
+++ interface/parametre.py
@@ -1,10 +1,10 @@
-### p.draw.rect => tracer un rectangle 
-blit => position
-if event.type == p.Keydown: 
-     if event.type == something
-p.mouse.get_pos()
- => click de la souris (voir bureau.py) 
-créer un nouveau fichier et le relier à game  
-Mission : Faire une page de paramètre qui interagit avec setting  ### 
+# p.draw.rect => tracer un rectangle 
+#blit => position
+#if event.type == p.Keydown: 
+#     if event.type == something
+#p.mouse.get_pos()
+# => click de la souris (voir bureau.py) 
+#créer un nouveau fichier et le relier à game  
+#Mission : Faire une page de paramètre qui interagit avec setting 
 
 

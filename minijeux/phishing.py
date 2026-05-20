03f08b71f14c12c0e0452777369865bdab953211
Léo <leo.tran0108@gmail.com>
2026-05-20 12:13:51 +0200

    modif

--- minijeux/phishing.py
+++ minijeux/phishing.py
@@ -2,6 +2,7 @@ import pygame as p
 import game as g
 import random
 from setting import Setting
+from data.missions import MAILS
 
 
 LIGNE_H  = 64
@@ -19,33 +20,19 @@ class Phishing():
         self.w, self.h = screen.get_width(), screen.get_height()
         panel_w = self.w - 400
         self.rect_panel = p.Rect(0, 0, panel_w, self.h)
-        self.rect_panel2 = p.Rect(50, HEADER_H, panel_w - 100, self.h - HEADER_H)
+        self.rect_panel2 = p.Rect(0, HEADER_H, panel_w, self.h - HEADER_H)
         self.rect_titre = p.Rect(MARGE, MARGE, panel_w - 2*MARGE, 30)
 
 
     def _items(self):
-        return list(self.position_actuelle.keys())
+        pass
 
     def _rect_item(self, index):
-        y = self.rect_liste.y + index * LIGNE_H - self.scroll_offset
-        return p.Rect(self.rect_liste.x, y, self.rect_liste.width, LIGNE_H - 3)
-
+        pass
     def _cliquer(self, nom):
-        valeur = self.position_actuelle[nom]
-
+        pass
     def handle_event(self, event, game):
-        if event.type == p.MOUSEBUTTONDOWN and event.button == 1:
-            items = self._items()
-            for i, nom in enumerate(items):
-                rect = self._rect_item(i)
-                if rect.collidepoint(event.pos) and self.rect_liste.collidepoint(event.pos):
-                    self._cliquer(nom)
-                    return
-
-        if event.type == p.MOUSEWHEEL:
-            if self.rect_panel.collidepoint(p.mouse.get_pos()):
-                self.scroll_offset = max(0, self.scroll_offset - event.y * LIGNE_H)
-
+        pass
     def update(self):
         pass
 
